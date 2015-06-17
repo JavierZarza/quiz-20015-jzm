cb010e72b5939bbd34ba96f454661055319a75b5
JavierZarza <jzarzame@hotmail.com>
2015-06-17 12:11:09 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -35,15 +35,19 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; //Exportar definición de la tabla Quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function () {
-    //success(..) ejecuta el manejador una vez creada la tabla
-    Quiz.count().success(function (count) {
+sequelize.sync().then(function () {
+    //then(..) ejecuta el manejador una vez creada la tabla
+    Quiz.count().then(function (count) {
         if(count  === 0){
             Quiz.create({
                 pregunta: 'Capital de Italia',
                 respuesta: 'Roma'
+            });
+            Quiz.create({
+                pregunta: 'Capital de Portugal',
+                respuesta: 'Lisboa'
             })
-                .success(function(){console.log('Base de Datos inicializada')});
+                .then(function(){console.log('Base de Datos inicializada')});
         };
     });
 });
